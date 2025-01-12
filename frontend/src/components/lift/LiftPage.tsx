c90dc887326607ca71783b295fefedf95bf4d891
Lilly Luo <lillyluo1995@gmail.com>
2025-01-12 18:48:04 -0500

    update

--- frontend/src/components/lift/LiftPage.tsx
+++ frontend/src/components/lift/LiftPage.tsx
@@ -26,29 +26,53 @@ const GET_LIFT_USING_ID = gql`
 
 export const LiftPage: React.FC = (): ReactElement => {
     const { id } = useParams()
-    const { loading, error, data } = useQuery(GET_LIFT_USING_ID, {  variables: { id: id ? parseInt(id) : "2" }});
+    const { loading, error, data } = useQuery<{getLiftUsingId: {
+        date: string, 
+        id: string, 
+        target_type: string, moves: {
+        move_metadata: {
+            name: string, target_muscle: string 
+        }, sets: {
+            set_count: number,
+            num_reps: number,
+            weight_lbs: number,
+            unilateral: boolean
+        }[]
+    }[]
+}}> 
+        (GET_LIFT_USING_ID, {  variables: { id: id ? parseInt(id) : "2" }});
     if (loading) return <div>Loading...</div>
     if (error) return <div>Error! {error.message}</div>
     return (<div>
       <div>Date: {data?.getLiftUsingId.date?.toString()}</div>
       <div>Target Type: {data?.getLiftUsingId.target_type}</div>
-        <table>
-            <thead>
-                <tr>
-                    <td>Move</td>
-                    <td>Num sets</td>
-                    <td>Set Details</td>
-                </tr>
-            </thead>
-            <tbody>
-            {data?.getLiftUsingId.moves.map((move) => (
-                <tr key={move.name}>
-                    <td>{move.move_metadata.name}</td>
-                    <td>3x5</td>
-                </tr>
+      <br/>
+            {data?.getLiftUsingId.moves.map((move) => {
+                return <div>
+                    <div>Move: {move.move_metadata.name}</div>
+                    <div>Type: {move.move_metadata.target_muscle}</div>
+                    <table>
+                    <thead>
+                        <tr>
+                            <td>Set Count</td>
+                            <td># Reps</td>
+                            <td>Weight (lbs)</td>
+                            <td>Unilateral</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {move.sets.map((set) => (
+                        <tr key={`${move.move_metadata.name}_${set.set_count}`}>
+                            <td>{set.set_count}</td>
+                            <td>{set.num_reps}</td>
+                            <td>{set.weight_lbs}</td>
+                            <td>{set.unilateral.toString()}</td>
+                        </tr>
             ))}
             </tbody>
-        </table>
+                </table>
+                </div>
+            })}
     </div>
     )
 }
